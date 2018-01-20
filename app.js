6857ee67550d9043d93d11e320828807e1ea286d
Samir Ketema <samir.ketema@gmail.com>
2018-01-19 18:04:36 -0800

    create express server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 var express = require('express');
+var debug = require('debug')('node-rules-engine');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+const LISTEN_PORT = 3000;
+
+var app = express();
+app.use(bodyParser.json());
+app.set('port', LISTEN_PORT);
+
+module.exports = app;
