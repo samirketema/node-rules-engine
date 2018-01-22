5577d661c85240c3767590916b51e294e0a5ddbd
Samir Ketema <samir.ketema@gmail.com>
2018-01-21 18:37:29 -0800

    rules cleanup

--- api/rules/controllers.js
+++ api/rules/controllers.js
@@ -68,25 +68,27 @@ exports.getRule = function (req, res) {
   overwrite that rule.
   Req: JSON representing a new rule to add to the rules registry
 */
-// exports.addRule = function (req, res) {
-//   if (!req.body) {
-//     return res.status(400).json({
-//       'message': 'No body provided in request.'
-//     });
-//   }
-//   let rule = req.body;
+/*
+exports.addRule = function (req, res) {
+  if (!req.body) {
+    return res.status(400).json({
+      'message': 'No body provided in request.'
+    });
+  }
+  let rule = req.body;
 
-//   //Validate top level of rule structure
-//   if (_.isString(rule.name) || _.isObject(rule.rule)) {
-//     return res.status(400).json({
-//       'message': 'rule structure invalid'
-//     });
-//   }
+  //Validate top level of rule structure
+  if (_.isString(rule.name) || _.isObject(rule.rule)) {
+    return res.status(400).json({
+      'message': 'rule structure invalid'
+    });
+  }
 
-//   //Add the rule
+  //Add the rule
 
-//   //Still in progress!
-// };
+  //Still in progress!
+};
+*/
 
 /*
   DELETE /rule
@@ -95,95 +97,6 @@ exports.getRule = function (req, res) {
 */
 exports.deleteRule = function (req, res) {
 };
-
-/*
-  Checks if a Operand is a rule (whether top-level or nested)
-  Input: Operand 
-  Output: boolean detailing if the operand is a rule.
-*/
-function isRule(obj) {
-  return _.isObject(obj) && obj.hasOwnProperty('operator') && obj.hasOwnProperty('operands');
-}
-
-/*
-  Evaluates an operand. A operand is a token that is either a rule, value, or field.
-  Input: Operand
-  Output: Boolean value
-
-  This utilizes two stacks, one to keep operands as they are evaluated (similar to polish notation),
-  and another to keep values which are determined from operands. This iterative approach uses less 
-  memory for deeply nested rules as it will not add more frames to the call stack when evaluating
-  different levels of rules.
-*/
-function evaluateRuleIterative (rule, userInput) {
-  let operators = Operator.retrieveOperators();
-  operandStack.push(rule);
-  
-  //Keep evaluating operands while we have them.
-  while (!_.isEmpty(operandStack)) {
-    //Get the current operand
-    let currElement = operandStack.pop();
-
-    //If the current operand is a rule, then push the operator and each of the operands onto the stack.
-    if (currElement.hasOwnProperty('operator') && currElement.hasOwnProperty('operands')) {
-      operandStack.push({
-        operator: currElement.operator,
-        size: currElement.operands.length
-      });
-      currElement.operands.forEach(function (o) {
-        operandStack.push(o);
-      });
-
-    //If the current operand is a value, push the value to the value stack.
-    } else if (currElement.hasOwnProperty('value')) {
-      if (!_.isString(currElement.value)) {
-        throw new Error ('Rule Structure invalid: value is not a string. value=' + currElement.value);
-      }
-      valueStack.push(currElement.value);
-
-    //If the current operand is a field, push the field to the value stack if found.
-    } else if (currElement.hasOwnProperty('field')) {
-      let field = lodash.get(userInput, currElement.field, false);
-      if (field === false) {
-        throw new Error('Field ' + currElement.field + ' not found in user input JSON');
-      }
-      valueStack.push(field);
-
-    //If the current operand is an operator, evaluate the operator against values in the value stack.
-    } else if (currElement.hasOwnProperty('operator')) {
-      if (valueStack.length < currElement.size) {
-        throw new Error('Rule structure invalid: not enough values for operator ' + currElement.operator);
-      }
-      let numParameters = operators[currElement.operator].numParameters;
-
-      // -1 denotes a flexible amount of parameters for the operator (e.g. OR)
-      if (numParameters !== -1 && numParameters !== currElement.size) {
-        throw new Error('Rule structure invalid: number of params !== number of. values=' + currElement.size + ', ' + currElement.operator +'=' + numParameters);
-      }
-      //Pop the desired number of values from value stack.
-      let values = [];
-      for (let i = 0; i < currElement.size; i++) {
-        values.push(valueStack.pop());
-      }
-      //push the result
-      valueStack.push(operators[currElement.operator].apply(values));
-    } else {
-      throw new Error('Rule structure invalid: incorrect operand structure');
-    }
-  }
-
-  //When the operand stack is empty, there should just be a single boolean value of whether or not the rule passed:
-  if (valueStack.length !== 1) {
-    throw new Error('Rule structure invalid: did not reduce down to single boolean value.')
-  }
-
-  let result = valueStack.pop();
-  if (!_.isBoolean(result)) {
-    throw new Error ('Rule validation error: Final value not a boolean. value=' + value);
-  }
-  
-  return result;
-}
 
 /*
   This function applies all rules in the rules registry against the user's input.
@@ -212,4 +125,117 @@ exports.applyRules = function (userInput) {
   } catch (e) {
     throw e;
   }
+};
+
+/*
+  Evaluates an operand. A operand is a token that is either a rule, value, or field.
+  Input: Operand
+  Output: Boolean value
+
+  This utilizes two stacks, one to keep operands as they are evaluated (similar to polish notation),
+  and another to keep values which are determined from operands. This iterative approach uses less 
+  memory for deeply nested rules as it will not add more frames to the call stack when evaluating
+  different levels of rules.
+*/
+function evaluateRuleIterative (rule, userInput) {
+  operandStack.push(rule);
+  
+  //Keep evaluating operands while we have them.
+  while (!_.isEmpty(operandStack)) {
+    //Get the current token
+    let currentToken = operandStack.pop();
+
+    //If the current operand is a rule, then push the operator and each of the operands onto the stack.
+    if (currentToken.hasOwnProperty('operator') && currentToken.hasOwnProperty('operands')) {
+      handleRule(currentToken);
+
+    //If the current operand is a value, push the value to the value stack.
+    } else if (currentToken.hasOwnProperty('value')) {
+      handleValue(currentToken);
+
+    //If the current operand is a field, push the field to the value stack if found.
+    } else if (currentToken.hasOwnProperty('field')) {
+      handleField(currentToken, userInput);
+
+    //If the current operand is an operator, evaluate the operator against values in the value stack.
+    } else if (currentToken.hasOwnProperty('operator')) {
+      handleOperator(currentToken);
+    } else {
+      throw new Error('Rule structure invalid: incorrect operand structure');
+    }
+  }
+
+  //When the operand stack is empty, there should just be a single boolean value of whether or not the rule passed:
+  if (valueStack.length !== 1) {
+    throw new Error('Rule structure invalid: did not reduce down to single boolean value.')
+  }
+
+  let result = valueStack.pop();
+  if (!_.isBoolean(result)) {
+    throw new Error ('Rule validation error: Final value not a boolean. value=' + value);
+  }
+
+  return result;
+};
+
+/*
+  Puts a rule's operator and operands onto the operand stack
+  Input: Rule
+*/
+function handleRule (rule) {
+  operandStack.push({
+    operator: rule.operator,
+    size: rule.operands.length
+  });
+  rule.operands.forEach(function (o) {
+    operandStack.push(o);
+  });
+};
+
+/*
+  Puts a value onto the value stack.
+  Input: Value object
+*/
+function handleValue (value) {
+  if (!_.isString(value.value)) {
+    throw new Error ('Rule Structure invalid: value is not a string. value=' + value.value);
+  }
+  valueStack.push(value.value);
+};
+
+/*
+  Puts a field from userInput onto the value stack.
+  Input: Field object
+*/
+function handleField (field, userInput) {
+  let value = lodash.get(userInput, field.field, false);
+  if (field === false) {
+    throw new Error('Field ' + field.field + ' not found in user input JSON');
+  }
+  valueStack.push(value);
+};
+
+/*
+  Applies an operator on values from the value stack.
+  Pushes the result onto the value stack.
+  Input: Operator object
+*/
+function handleOperator (operator) {
+  let operators = Operator.retrieveOperators();
+  if (valueStack.length < operator.size) {
+    throw new Error('Rule structure invalid: not enough values for operator ' + operator.operator);
+  }
+  let numParameters = operators[operator.operator].numParameters;
+
+  // -1 denotes a flexible amount of parameters for the operator (e.g. OR)
+  if (numParameters !== -1 && numParameters !== operator.size) {
+    throw new Error('Rule structure invalid: number of params !== number of. values=' + operator.size + ', ' + operator.operator +'=' + numParameters);
+  }
+  //Pop the desired number of values from value stack.
+  let values = [];
+  for (let i = 0; i < operator.size; i++) {
+    values.push(valueStack.pop());
+  }
+  //push the result
+  valueStack.push(operators[operator.operator].apply(values));
 };
